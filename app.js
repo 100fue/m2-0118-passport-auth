8afefd0a04d28885c0c17fa0a213993d5a8e6c23
Marc Pomar <marc@faable.com>
2018-02-05 13:48:38 +0100

    done

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const debug = require('debug')(`m2-0118-passport-auth:${path.basename(__filename).split('.')[0]}`)
+const passportConfig = require('./passport')
+const {dbURL} = require('./config');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+mongoose.connect(dbURL)
+        .then(() => debug(`Connected to ${dbURL}`))
+        .catch(e => console.log(e))
 
-var app = express();
+
+const index = require('./routes/index');
+const auth = require('./routes/auth');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,13 +28,29 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+passportConfig(app);
+
+app.use((req,res,next) => {
+  res.locals.user = req.user;
+  res.locals.title = 'Passport Auth 0118';
+  next();
+}) 
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
